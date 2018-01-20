0bf8a90e5f6f791bccdd5cbc6ec434ff629f087c
vagrant <vagrant@homestead>
2018-01-20 07:51:37 +0000

    topics

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -16,16 +21,19 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request,ImageUploadHandler $uploader,User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
-        $data=$request->all();
-        if ($request->avatar){
+        $this->authorize('update', $user);
+        $data = $request->all();
+
+        if ($request->avatar) {
             $result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
-            if ($result){
-                $data['avatar']=$result['path'];
+            if ($result) {
+                $data['avatar'] = $result['path'];
             }
         }
 
